e286bcb4a837fee62d465257d1c51bb87f6fa88b
jadeye <newebgen@gmail.com>
2011-09-30 14:32:43 +0300

    Upgrading man class

---
+++ src/inters/createWoman.java
@@ -0,0 +1,13 @@
+package inters;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jud
+ * Date: 9/30/11
+ * Time: 2:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface createWoman {
+
+    String getClsName();
+}
