f18e992891220e5cab74e2c03e4a8781ee33d61f
jadeye <newebgen@gmail.com>
2011-10-02 11:56:37 +0200

    Fruit in Basket...?...YES!

---
+++ src/fruit/ClassUtils.java
@@ -0,0 +1,55 @@
+package fruit;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jud
+ * Date: 9/30/11
+ * Time: 12:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClassUtils {
+    // returns the class (without the package if any)
+    public static String getClassName(Class c) {
+        String FQClassName = c.getName();
+        int firstChar;
+        firstChar = FQClassName.lastIndexOf('.') + 1;
+        if (firstChar > 0) {
+            FQClassName = FQClassName.substring(firstChar);
+        }
+        return FQClassName;
+    }
+
+
+    // returns package and class name
+    public static String getFullClassName(Class c) {
+        return c.getName();
+    }
+
+    // returns the package without the classname, empty string if
+    // there is no package
+    public static String getPackageName(Class c) {
+        String fullyQualifiedName = c.getName();
+        int lastDot = fullyQualifiedName.lastIndexOf('.');
+        if (lastDot == -1) {
+            return "";
+        }
+        return fullyQualifiedName.substring(0, lastDot);
+    }
+
+    /*
+  public static void main(String[] args) {
+    System.out.println(ClassUtils.getClassName(java.awt.Frame.class));
+    System.out.println(ClassUtils.getFullClassName(java.awt.Frame.class));
+    System.out.println(ClassUtils.getPackageName(java.awt.Frame.class));
+    System.out.println("----");
+    System.out.println(ClassUtils.getClassName(ClassUtils.class));
+    System.out.println(ClassUtils.getFullClassName(ClassUtils.class));
+    System.out.println(ClassUtils.getPackageName(ClassUtils.class));
+    System.out.println("----");
+    java.util.Calendar cal = java.util.Calendar.getInstance();
+    System.out.println(ClassUtils.getClassName(cal.getClass()));
+    System.out.println(ClassUtils.getFullClassName(cal.getClass()));
+    System.out.println(ClassUtils.getPackageName(cal.getClass()));
+    }
+    */
+}
--- src/fruit/basket.java
+++ src/fruit/basket.java
@@ -1,8 +1,7 @@
 package fruit;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,46 +10,66 @@ import java.util.List;
  * Time: 1:53 PM
  * To change this template use File | Settings | File Templates.
  */
-public class basket{
+public class basket {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<String> str = new ArrayList<String>();
         str.add("Hello ");
         str.add("World.");
-        String MyString ="";
+        String MyString = "";
         /*
         String MyString = str.get(0);
 
         System.out.println(MyString);
         System.out.println(str.get(0)+str.get(1));
         */
-        for (Iterator<String> iter=str.iterator(); iter.hasNext();){
+        for (Iterator<String> iter = str.iterator(); iter.hasNext(); ) {
             //String s= iter.next();
             MyString = MyString.concat(iter.next());
-            }
+        }
         System.out.println(MyString);
 
 
         List<Integer> ints = new ArrayList<Integer>();
-            ints.add(0);
-            ints.add(1);
+        ints.add(0);
+        ints.add(1);
 
-            int sum = 0;
-            for (int i : ints) {
+        int sum = 0;
+        for (int i : ints) {
             sum += i;
-            }
+        }
 
-//        List<Apple> apples = new ArrayList<Apple>();
-//        List<? extends Fruit> fruits = apples;
+        /*List<Apple> apples = new ArrayList<Apple>();
+        List<? extends Fruit> fruits = apples;
 
-        //Apple[] apples = new Apple[1];
-        //Fruit[] fruits = apples;
-        //fruits[0] = new Strawberry();
-        /*
+        Apple[] apples = new Apple[1];
+        Fruit[] fruits = apples;
+        fruits[0] = new Strawberry();
+
         List<Apple> apples = new ArrayList<Apple>();
         List<? extends Fruit> fruits = apples;
         fruits.add(new Strawberry());
-          */
+        */
+        Collection<Fruit> fruits = new HashSet<Fruit>();
+        fruits.add(new Strawberry());
+        fruits.add(new Apple());
+
+
+        for (Iterator<Fruit> itera1 = fruits.iterator(); itera1.hasNext(); ) {
+            //String s= iter.next();
+            Fruit MyFruit = itera1.next();
+            MyFruit.name = ClassUtils.getClassName(MyFruit.getClass());
+            if (MyFruit.name.equals("Apple"))
+                MyFruit.setColor(1);
+            else
+                MyFruit.setColor(0);
+//            if ()
+            System.out.println(MyFruit.name + " is " + MyFruit.color);
+            /*else
+                System.out.println("and a "+MyFruit.color+" "+MyFruit.name);*/
+//            System.out.println(MyFruit);
+//            System.out.println(MyFruit.color);
+        }
 
 
     }
